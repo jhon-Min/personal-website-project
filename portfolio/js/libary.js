ab08ae18a0b1b1802040d6ad1cca6f3503d3c2f9
Seo Jun Min <minnyisay@gmail.com>
2021-05-16 20:11:46 +0630

    edit portfolio project

---
+++ portfolio/js/libary.js
@@ -0,0 +1,27 @@
+// ===== Venobox =====
+$(document).ready(function(){
+    $('.venobox').venobox({
+        framewidth : '400px',                            // default: ''
+        frameheight: 'auto',                            // default: ''
+        border     : '2px',                             // default: '0'
+        bgcolor    : '#ffffff',                          // default: '#fff'
+        titleattr  : 'data-title',                       // default: 'title'
+        numeratio  : true,                               // default: false
+        infinigall : true,                               // default: false
+        share: ['facebook', 'twitter', 'download'], // default: []
+        spinner: 'rotating-plane',
+        spinColor: ' #695aa6'
+    }); 
+});
+
+//======== WOW JS =========
+  wow = new WOW(
+  {
+  boxClass:     'wow',      // default
+  animateClass: 'animate__animated', // default
+  offset:       0,          // default
+  mobile:       true,       // default
+  live:         true        // default
+}
+)
+wow.init();
